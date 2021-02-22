6b58f87dbe10234837c1b9dbb7478dd957fd5f28
Elinvisible <unenes.viejos@gmail.com>
2021-02-22 10:26:39 -0600

    se trabajo en el roteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ResultadoComponent } from './components/resultado/resultado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Los que estan en llaves son los componentes, el InicioCOmponent es la clase
+  { path: '', component: InicioComponent},
+  { path: 'resultado', component: ResultadoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
